fa92cfefdc210f909e419a7387cb9cd68b2adac7
Philippe Schmouker <ph.schmouker@yahoo.fr>
2025-08-07 14:33:21 +0200

    #26-refactor colors definitions
    Completed. Tested.

--- ArcheryTrainingTimer/app/src/main/java/com/example/archerytrainingtimer/ui/theme/Color.kt
+++ ArcheryTrainingTimer/app/src/main/java/com/example/archerytrainingtimer/ui/theme/Color.kt
@@ -2,16 +2,22 @@ package com.example.archerytrainingtimer.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-// Define your app-specific colors here
+val WAYellowColor = Color(0xFFFFE552) // Yellow color used in the app
+val WARedColor = Color(0xFFF65058) // Red color used in the app
+val WABlueColor = Color(0xFF00B4E4) // Blue color used in the app
+val WABlackColor = Color(0xFF000000)
+val WAWhiteColor = Color(0xFFFFFFFF)
+
 val AppBackgroundColor = Color(0xFF2F3133)
 val AppTimerRowBackgroundColor = Color(0xFF111213)
-val AppTextColor = Color(0xFFFFF5F2)
-val AppTitleColor = Color(0xFFFFE552)
+val AppTextColor = WAWhiteColor
+val AppTitleColor = WAYellowColor
 val AppButtonColor = Color(0xFF00B4E4)
 val AppButtonTextColor = Color(0xFF33373F)
 
 val SelectedButtonBackgroundColor = AppTitleColor // Background for selected button
-val SelectedButtonBorderColor = Color(0xFFF65058)
+val SelectedButtonBorderColor = WARedColor
+val ProgressBorderColor = Color(0xFF923035) // i.e 60% of WARedColor
 
 val AppSurfaceContainerLow = Color(0xFF08090A)
 val AppSurfaceContainerHigh = Color(0xFF212325)
