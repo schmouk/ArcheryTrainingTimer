984f7cf1062bf36263e43a404ee7d9e1aee8e358
Philippe Schmouker <ph.schmouker@yahoo.fr>
2025-12-10 22:19:34 +0100

    #205-create module SessionChoiceViewModel.kt
    Done. Compiles. Still to be validated.

---
+++ ArcheryTrainingTimer/app/src/main/java/com/github/schmouk/archerytrainingtimer/sessionchoice/SessionChoiceViewModel.kt
@@ -0,0 +1,57 @@
+/*
+MIT License
+
+Copyright (c) 2025 Philippe Schmouker, ph (dot) schmouker (at) gmail (dot) com
+
+This file is part of Android application ArcheryTrainingTimer.
+
+Permission is hereby granted,  free of charge,  to any person obtaining a copy
+of this software and associated documentation files (the "Software"),  to deal
+in the Software without restriction,  including without limitation the  rights
+to use,  copy,  modify,  merge,  publish,  distribute, sublicense, and/or sell
+copies of the Software,  and  to  permit  persons  to  whom  the  Software  is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS",  WITHOUT WARRANTY OF ANY  KIND,  EXPRESS  OR
+IMPLIED,  INCLUDING  BUT  NOT  LIMITED  TO  THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT  SHALL  THE
+AUTHORS  OR  COPYRIGHT  HOLDERS  BE  LIABLE  FOR  ANY CLAIM,  DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,  ARISING FROM,
+OUT  OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+package com.github.schmouk.archerytrainingtimer.sessionchoice
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.github.schmouk.archerytrainingtimer.commons.SessionType
+import com.github.schmouk.archerytrainingtimer.commons.UserPreferencesRepository
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
+
+class SessionChoiceViewModel(
+    private val userPreferencesRepository: UserPreferencesRepository
+) : ViewModel() {
+
+    // Expose the selected session type as a StateFlow for the UI to observe.
+    // Default to null initially.
+    val selectedSessionType: StateFlow<SessionType?> =
+        userPreferencesRepository.sessionType.stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(3_500),
+            initialValue = null
+        )
+
+    // Function for the UI to call when a session type is selected.
+    fun selectSessionType(sessionType: SessionType) {
+        viewModelScope.launch {
+            userPreferencesRepository.saveSessionType(sessionType)
+        }
+    }
+}
